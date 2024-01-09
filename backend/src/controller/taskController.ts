634e7a4a0b0f89cad0baa489bb46866296b3eb61
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-09 00:29:05 -0800

    Add task controller for CRUD operations

---
+++ backend/src/controller/taskController.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+import Task from "../model/task";
+type task = {
+  _id?: string;
+  text: string;
+  day: string;
+  reminder: boolean;
+};
+const fetchTasks = async (req: Request, res: Response) => {
+  try {
+    const tasks: task[] = await Task.find();
+    res.json(tasks);
+  } catch (error : any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const fetchTask = async (req: Request, res: Response) => {
+  try {
+    const task: task | null = await Task.findById(req.params.id);
+    if (task) {
+      res.json(task);
+    } else {
+      res.status(404).json({ message: "Task not found" });
+    }
+  } catch (error : any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const createTask = async (req: Request, res: Response) => {
+    const task: task = new Task({
+        text: req.body.text,
+        day: req.body.day,
+        reminder: req.body.reminder,
+    });
+    try {
+        const newTask = await task.save();
+        res.status(201).json(newTask);
+    } catch (error : any) {
+        res.status(400).json({ message: error.message });
+    }
+};
+const updateTask = async (req: Request, res: Response) => {
+  const { text,date,reminder } = req.body;
+  try {
+    const task = await Task.findById(req.params.id);
+    if (task) {
+      task.text = text;
+      task.day = date;
+      task.reminder = reminder;
+      const updatedTask = await task.save();
+      res.json(updatedTask);
+    } else {
+      res.status(404).json({ message: "Task not found" });
+    }
+  } catch (error : any) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const deleteTask = async (req: Request, res: Response) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (task) {
+      await task.remove();
+      res.json({ message: "Task deleted" });
+    } else {
+      res.status(404).json({ message: "Task not found" });
+    }
+  } catch (error : any) {
+    res.status(500).json({ message: error.message });
+  }
+};
