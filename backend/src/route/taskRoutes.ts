b0030f5ea0ce5a9f7d0acab8e86bb8ba9658a52d
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-09 11:21:33 -0800

    change route variable into taskRouter

--- backend/src/route/taskRoutes.ts
+++ backend/src/route/taskRoutes.ts
@@ -1,10 +1,10 @@
 import express, {Router} from 'express'
 import { fetchTask,fetchTasks,createTask,updateTask,deleteTask } from '../controller/taskController'
-const route:Router = express.Router() 
-route.get('/task/:id',fetchTask)
-route.get('/tasks',fetchTasks)
-route.post('/task',createTask)
-route.put('/task/:id',updateTask)
-route.delete('/task/:id',deleteTask)
+const taskRouter:Router = express.Router() 
+taskRouter.get('/task/:id',fetchTask)
+taskRouter.get('/tasks',fetchTasks)
+taskRouter.post('/task',createTask)
+taskRouter.put('/task/:id',updateTask)
+taskRouter.delete('/task/:id',deleteTask)
 
-export default route
+export default taskRouter
