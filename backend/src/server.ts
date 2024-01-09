cf49688a14e5d259b0637bde62b93261686b6f07
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-09 11:20:54 -0800

    import taskRouter from routes folder

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import taskRouter from './route/taskRoutes'
 dotenv.config()
 const PORT = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/api',taskRoutes)
+app.use('/api',taskRouter)
 
 
 app.listen(PORT, () => {
