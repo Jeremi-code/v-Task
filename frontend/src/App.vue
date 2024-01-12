dbfde1b858ca58997577208be60750f822ea5711
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-11 23:44:54 -0800

    Add task component and styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,40 @@
 <script setup lang="ts">
+import { TrackOpTypes } from 'vue';
+import {task} from './types/TaskType'
+  name : 'Task'
+  props : defineProps< {
+    'task' : task,
+    'onDelete' :any ,
+    'onToggle' : any ,
+  }> ();
+  setup() {
+
+  }
 </script>
+
 <template>
-  <div>
-    <p>does this work</p>
-    
-  </div>
+  <div :class="task.reminder ? reminder  : "">
+  <h3>{{ task.text }} X</h3>
+  <p>{{ task.day }}</p>
+</div>
 </template>
 
 <style scoped>
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.task.reminder {
+  border-left: 5px solid green;
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 
 </style>
