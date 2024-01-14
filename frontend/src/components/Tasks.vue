be14fb2c4c4683ce2445e2545ee7003de6aa5a57
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-14 00:27:45 -0800

    import task component inside task component

--- frontend/src/components/Tasks.vue
+++ frontend/src/components/Tasks.vue
@@ -1,10 +1,18 @@
 <template>
-  
+    <div>
+        <div v-for="task in tasks">
+            <Task/>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Task from "./Task.vue"
 export default defineComponent ({
+    name : 'Tasks',
+    components: {Task},
+    
 
 })
 </script>
