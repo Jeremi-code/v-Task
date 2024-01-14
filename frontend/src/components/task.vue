12c8ff85ffcc31de855ebd7a2a7be6ead3244976
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-14 00:28:32 -0800

    change some functions and template codes

--- frontend/src/components/task.vue
+++ frontend/src/components/task.vue
@@ -1,12 +1,49 @@
 <template>
+  <div :class="{task:true , reminder:task.reminder}">
+    <h3>{{ task.text }}</h3>
+    <p></p>
+  </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { task } from "../types/TaskType";
+import { defineComponent, PropType } from "vue";
+export default defineComponent ({
+  name : 'Task',
+  props : {
+    task : {
+      required : true,
+      type : Array as PropType<task[]>
+    },
+    onToggle : {
+      required : true,
+      type : Function
+    },
+    onDelete : {
+      required : true ,
+      type : Function
 
-}
+  }
+  }
+})
+
 </script>
 
 <style>
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
 
+.task.reminder {
+  border-left: 5px solid green;
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
