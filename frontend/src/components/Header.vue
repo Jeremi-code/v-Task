b621c34ce7801fe490083ae80827fa97a2073b27
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-14 03:04:43 -0800

    create a header component for the header text and the button

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <h1>Task Tracker</h1>
+        <Button :onClick="onAdd" :title="showAdd ? 'Close' : 'Add'" :color = "showAdd ? 'red' : 'green'"></Button>
+    </header>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+    import Button from "./Button.vue"
+    export default defineComponent({
+        name : 'Header',
+        component : {Button},
+        props : {
+            onAdd : Function,
+            showAdd : Boolean
+        }
+    })
+    
+
+</script>
+
+<style>
+
+</style>
