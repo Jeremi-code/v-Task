4988fbb88424c5f64eff6a4b834e73c251ed2c6c
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-15 04:18:54 -0800

    import the customButton inside header component

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,16 +1,17 @@
 <template>
     <header class = "header">
         <h1>Task Tracker</h1>
-        <Button :onClick="onAdd" :title="showAdd ? 'Close' : 'Add'" :color = "showAdd ? 'red' : 'green'"></Button>
+        <CustomButton :onClick="onAdd" :title="showAdd ? 'Close' : 'Add'" :color = "showAdd ? 'red' : 'green'"/>
+        
     </header>
 </template>
 
 <script lang="ts">
     import { defineComponent } from "vue";
-    import Button from "./Button.vue"
+    import CustomButton from "./CustomButton.vue"
     export default defineComponent({
         name : 'Header',
-        component : {Button},
+        components : {CustomButton},
         props : {
             onAdd : Function,
             showAdd : Boolean
