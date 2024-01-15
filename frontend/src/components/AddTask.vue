bd4be7cdc01192b3e8870e0b83255665e0c54b32
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-15 06:19:30 -0800

    add reactive datas that track the inserted text,day and reminder

--- frontend/src/components/AddTask.vue
+++ frontend/src/components/AddTask.vue
@@ -2,7 +2,7 @@
     <form class="add-form" @submit="onSubmit">
         <div class="form-control">
             <label>Task</label>
-            <input type="text"/>
+            <input type="text" v-model=""/>
         </div>
         <div class="form-control">
             <label>Day & Time</label>
@@ -10,17 +10,21 @@
         </div>
         <div class="form-control">
             <label>Set Reminder</label>
-            <input type="checkbox">
+            <input type="checkbox" :checked="reminder" value="reminder" >
         </div>
         <button>Save Task</button>
     </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
     setup() {
-        
+        const text = ref<String | null>(null)
+        const day = ref<String | null>(null)
+        const reminder = ref<boolean>(false)
+
+        return {text,day,reminder}
     },
 })
 </script>
