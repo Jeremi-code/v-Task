00582c979919e52fcf7e60288befd826c6aff8ff
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-15 00:26:50 -0800

    create a component for adding a task

---
+++ frontend/src/components/AddTask.vue
@@ -0,0 +1,26 @@
+<template>
+    <form class="add-form" @submit="onSubmit">
+        <div class="form-control">
+            <label>Task</label>
+            <input type="text"/>
+        </div>
+        <div class="form-control">
+            <label>Day & Time</label>
+            <input type="text"/>
+        </div>
+        <div class="form-control">
+            <label>Set Reminder</label>
+            <input type="checkbox">
+        </div>
+        <button>Save Task</button>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
