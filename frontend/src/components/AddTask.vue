cdb4785de5bbba3ee109abe43beba617bfbc899f
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-16 00:26:11 -0800

    add functions that will change the value of refs

--- frontend/src/components/AddTask.vue
+++ frontend/src/components/AddTask.vue
@@ -2,29 +2,48 @@
     <form class="add-form" @submit="onSubmit">
         <div class="form-control">
             <label>Task</label>
-            <input type="text" v-model=""/>
+            <input type="text" @change="setText(($event.target as HTMLInputElement).value)" v-model="text">
         </div>
         <div class="form-control">
             <label>Day & Time</label>
-            <input type="text"/>
+            <input type="text" @change="setDate(($event.target as HTMLInputElement).value)" v-model="day"/>
         </div>
         <div class="form-control">
             <label>Set Reminder</label>
-            <input type="checkbox" :checked="reminder" value="reminder" >
+            <input type="checkbox" @change="setReminder(($event.target as HTMLInputElement).checked)" v-model="reminder" >
         </div>
         <button>Save Task</button>
-    </div>
+    </form>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
+    // props:{
+        
+    // }
     setup() {
         const text = ref<String | null>(null)
         const day = ref<String | null>(null)
         const reminder = ref<boolean>(false)
 
-        return {text,day,reminder}
+        const setText = (target:string) => {
+            text.value = target
+        }
+        const setDate = (target:string) => {
+            day.value = target
+        }
+        const setReminder = (target:boolean) => {
+            reminder.value = target 
+        }
+        const onSubmit = (event:Event) => {
+            event.preventDefault()
+            // onAdd({text : text.value,day : day.value , reminder : reminder.value})
+            setText('')
+            setDate('')
+            setReminder(false)
+        }
+        return {text,day,reminder,setText,setDate,setReminder,onSubmit}
     },
 })
 </script>
