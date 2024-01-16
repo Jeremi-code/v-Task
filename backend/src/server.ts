bdc02efaa802bcbd5f061eea179dc92e5b905b68
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-16 06:11:35 -0800

    Add database connection and configuration
    This commit adds a database connection and configuration to the server code. The `connectDB()` function is imported and called to establish a connection to the database.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import taskRouter from './route/taskRoutes'
+import connectDB from './config/db'
 dotenv.config()
+connectDB()
 const PORT = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
