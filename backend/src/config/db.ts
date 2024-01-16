c419976763120ef4c01ffe331dc8dfd89a2d735d
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-16 06:08:48 -0800

    Add check for MONGO_URI in connectDB function

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
+    if(!process.env.MONGO_URI) throw new Error("No MONGO_URI provided");
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error: any) {
