be9634dc458f9565b62bccae1b10273fef54f9ce
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-20 01:23:36 -0800

    import the task type from taskType.ts

--- frontend/src/components/Tasks.vue
+++ frontend/src/components/Tasks.vue
@@ -9,11 +9,12 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
 import Task from "./Task.vue"
+import { task } from '../types/TaskType';
 export default defineComponent ({
     name : 'Tasks',
     components: {Task},
     props : {
-        Tasks : Array as PropType<Task[]>
+        Tasks : Array as PropType<task[]>
     }
     
 
