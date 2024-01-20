e93cad3f80c952eabdef8edfd50e4f2c6950a8c4
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-20 02:08:26 -0800

    change the port of api

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import Task from "./components/Task.vue"
+import Tasks from "./components/Tasks.vue"
 import Header from "./components/Header.vue"
 import { defineComponent } from "vue";
 import { task } from "./types/TaskType";
@@ -7,17 +7,18 @@ import {onMounted,ref } from "vue";
 import AddTask from "./components/AddTask.vue";
 export default defineComponent ({
   name : 'App',
-  components : { Task, Header, AddTask },
+  components : { Tasks, Header, AddTask },
   setup() {
     let tasks = ref<task[] | null> (null)
     const showAdd = ref<boolean>(false)
     const fetchTasks = async () => {
-       const res = await fetch("http://localhost:5173/tasks")
+       const res = await fetch("http://localhost:5000/api/tasks", {
+       })
        const data : task[] = await res.json()
        return data
     }
     const addTasks = async (task:task) => {
-      const res : any = await fetch("http://localhost:5173/task",{
+      const res : any = await fetch("http://localhost:5000/task",{
         method : 'POST',
         headers : {
           'Content-Type':'application/json'
@@ -29,7 +30,7 @@ export default defineComponent ({
     }
 
     const deleteTask = async (id : string) => {
-       await fetch(`http://localhost:5173/task/${id}`, {
+       await fetch(`http://localhost:5000/task/${id}`, {
         method : 'DELETE'
       })
       if (tasks.value !== null) {
@@ -37,12 +38,12 @@ export default defineComponent ({
       }
     }
     const fetchTask = async (id:string) => {
-      const res = await fetch(`http://localhost:5173/task/${id}`)
+      const res = await fetch(`http://localhost:5000/task/${id}`)
       const data = await res.json()
       return data
     }
     const toggleReminder = async (id:string) => {
-      const res = await fetch(`http://localhost:5173/task/${id}`, {
+      const res = await fetch(`http://localhost:5000/task/${id}`, {
         method : 'PUT'
       })
       const data : task = await res.json()
@@ -54,13 +55,14 @@ export default defineComponent ({
     const toggleShowAdd = () => {
       showAdd.value = !showAdd.value
     }
-    onMounted(async () => {
+    onMounted( () => {
       const getTasks = async () => {
         const data = await fetchTasks()
         tasks.value=data
+        console.log(data,'inin')
       }
-
-      await getTasks()
+       getTasks()
+      console.log(tasks)
 
     })
     return {fetchTasks,fetchTask,toggleReminder,deleteTask,tasks,addTasks,showAdd,toggleShowAdd}
@@ -72,6 +74,7 @@ export default defineComponent ({
   <div class="container">
     <Header :showAdd="showAdd" :onAdd="toggleShowAdd"/>
     <AddTask v-if="showAdd" :addTask="addTasks" />
+    <Tasks v-if="tasks?.values " :Tasks="tasks"/>
   </div>
 </template>
 
