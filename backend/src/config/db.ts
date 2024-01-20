0a95d1af29d191bd8f2615553da4a86435c57ae4
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-19 23:33:56 -0800

    use the connection string directly to solve other problems

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,8 +4,8 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    if(!process.env.MONGO_URI) throw new Error("No MONGO_URI provided");
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    // if(!process.env.MONGO_URI) throw new Error("No MONGO_URI provided");
+    const conn = await mongoose.connect("mongodb://localhost:27017/TaskManager");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error: any) {
     console.error(`Error: ${error.message}`);
