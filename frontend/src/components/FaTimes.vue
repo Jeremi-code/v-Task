8240fdfc940ee44029fa19a3489149d0eff77252
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-24 11:07:27 -0800

    create a component that resembles like Fatimes

---
+++ frontend/src/components/FaTimes.vue
@@ -0,0 +1,10 @@
+<template>
+    <div style="color:red">&times;</div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+    export default defineComponent ({
+        name : 'FaTimes'
+    })
+</script>
