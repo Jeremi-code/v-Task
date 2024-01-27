7e6baf4cf08c554bb536632284f5d0a2a39fc6d5
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-27 03:24:05 -0800

    Add Done_Tasks and Home views

---
+++ frontend/src/views/Done_Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+ <h1>Done Tasks</h1> 
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Done_Tasks",
+  setup() {
+    return {};
+  },
+});
+
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>ahahhahahhah</h1>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style>
+
+</style>
