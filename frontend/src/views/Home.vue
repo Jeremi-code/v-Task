b66fdc067c64ecb6ea092d9aa7df661fe8cca93c
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-27 04:39:17 -0800

    Refactor Home.vue component

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,40 @@
 <template>
-  <h1>ahahhahahhah</h1>
+  <div>
+    <Tasks v-if="tasks?.values " :Tasks="tasks" :onToggle="toggleReminderHandler" :onDelete="deleteTaskHandler"/>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
-
+import { ref, onMounted } from "vue";
+import { task } from "../types/TaskType";
+import {deleteTask, fetchTasks, toggleReminder} from '../services/ApiService'
+import AddTask from "../components/AddTask.vue";
+import Tasks from "../components/Tasks.vue";
 export default defineComponent({
+  components: { AddTask,Tasks },
   name: "Home",
   setup() {
-    return {};
+    let tasks = ref<task[] | null>(null);
+    const toggleReminderHandler = async (id : string) => {
+      const data = await toggleReminder(id);
+      tasks.value = tasks.value!.map((task: task) =>
+        task._id === id ? { ...task, reminder: data.reminder } : task
+      );
+    }
+    const deleteTaskHandler = async (id:string) => {
+      await deleteTask(id);
+      tasks.value = tasks.value!.filter((task) => task._id !== id);
+    };
+
+    onMounted(async () => {
+      const getTasks = async () => {
+        const data = await fetchTasks('undone' as string);
+        tasks.value = data;
+      };
+      getTasks();
+    });
+    return {toggleReminderHandler,tasks,deleteTaskHandler};
   },
 });
 </script>
