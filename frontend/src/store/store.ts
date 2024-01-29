e9bc62c6d3c233dbd3c26d852597b1348acdcb19
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-28 22:59:27 -0800

    Add store module for managing showAdd state

---
+++ frontend/src/store/store.ts
@@ -0,0 +1,20 @@
+import { createStore } from "vuex";
+
+export default createStore({
+    state : {
+        showAdd : false,
+    },
+    mutations : {
+        toggleShowAdd(state){
+            state.showAdd = !state.showAdd
+        }
+    },
+    actions : {
+        toggleShowAdd({commit}){
+            commit('toggleShowAdd')
+        }
+    },
+    getters : {
+        showAdd : (state) => state.showAdd
+    }
+});
