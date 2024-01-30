68f8bf2da7743dc5e082e64371299c21a8b6c253
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-30 11:12:47 -0800

    make this page to fetch the done tasks on mounted

--- frontend/src/views/Done_Tasks.vue
+++ frontend/src/views/Done_Tasks.vue
@@ -1,14 +1,30 @@
 <template>
- <h1>Done Tasks</h1> 
+  <div>
+    <Tasks v-if="tasks?.values" :Tasks="tasks" :onToggle="onToggle"/>
+  </div>
 </template>
 
-<script>
-import { defineComponent } from "vue";
-
+<script lang="ts">
+import { defineComponent, onMounted,ref } from "vue";
+import Tasks from "../components/Tasks.vue"
+import { fetchTasks } from "../services/ApiService";
+import {task} from "../types/TaskType"
 export default defineComponent({
   name: "Done_Tasks",
+  components: {Tasks},
   setup() {
-    return {};
+    let tasks = ref<task[] | null>(null)
+    const onToggle= () => {
+      console.log('just for now')
+    }
+    onMounted (async () => {
+      const getTasks = async () => {
+        const data = await fetchTasks('Done' as string)
+        tasks.value = data
+      };
+      getTasks();
+    })
+    return {tasks,onToggle}
   },
 });
 
