b555636a78c5c58cee05b17af1d5f3385c1390c8
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-31 00:20:05 -0800

    Add Undo component

---
+++ frontend/src/components/Undo.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg width="22" height="22">
+    <path
+      d="M13.5 3C8.81 3 5 6.81 5 11.5S8.81 20 13.5 20 22 16.19 22 11.5 18.19 3 13.5 3zm0 16c-3.59 0-6.5-2.91-6.5-6.5S9.91 6 13.5 6 20 8.91 20 12.5 17.09 20 13.5 20zM11 9H7v4h4c1.1 0 2-.9 2-2s-.9-2-2-2z"
+    />
+  </svg>
+</template>
+<script lang="ts">
+import { defineComponent} from 'vue';
+export default defineComponent({
+  name: 'Undo',
+  props: {},
+  setup() {
+    return {};
+  },
+});
+</script>
