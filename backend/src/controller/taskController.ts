262265ebdaf1eb39d1a2743836bebcdac45a658f
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-31 23:29:57 -0800

    Refactor taskController.ts to remove commented code and optimize task retrieval

--- backend/src/controller/taskController.ts
+++ backend/src/controller/taskController.ts
@@ -4,14 +4,12 @@ import {DTask} from "../types/taskType"
 
 const fetchTasks = async (req: Request, res: Response) => {
   try {
-    // console.log('what is going on here')
     const status = req.query.status;
     if (status === 'Done') {
       const tasks: DTask[] | null = await Task.find({isCompleted:true});
       res.json(tasks);
     } else if (status === 'undone') {
       const tasks: DTask[] | null = await Task.find({isCompleted:'false'});
-      // const tasks : DTask[] | null = await Task.find({text : 'go to university'})
       res.json(tasks);
     }
   } catch (error : any) {
@@ -50,17 +48,12 @@ const updateTask = async (req: Request, res: Response) => {
   try {
     const task : DTask | null = await Task.findById(req.params.id);
     if (task) {
-      // task.text = text || task.text;
-      // task.day = date || task.day;
-      // task.reminder = reminder || task.reminder;
-      // console.log(isCompleted,reminder)
       if (isCompleted !== undefined) {
         task.isCompleted = !task.isCompleted;
       }
       if (reminder !== undefined) {
         task.reminder = !task.reminder;
       }
-      // task.reminder = !task.reminder
       const updatedTask = await task.save();
       res.status(201).json(updatedTask);
     } else {
