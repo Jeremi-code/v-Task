5f4e69928d10425b091a72fef1ceb2a80d4708ea
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-02-01 01:03:48 -0800

    Add active class to current route link

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@
     <Header  />
     <div class="contents">
       <div class="links">
-        <router-link to="/">Home</router-link>
+        <router-link to="/" :class="{'active' : routeValue}">Home</router-link>
         <router-link to="/done_tasks">Done_Tasks</router-link>
       </div>
       <router-view></router-view>
@@ -12,11 +12,23 @@
 </template>
 
 <script lang="ts">
-import { defineComponent} from "vue";
+import { computed, defineComponent} from "vue";
+import { useRouter } from "vue-router";
 import Header from "./components/Header.vue";
 export default defineComponent ({
     name : 'App',
     components : {Header},
+    setup() {
+      const router = useRouter()
+      const currentRoute = computed(() => router.currentRoute.value.path)
+      let routeValue:boolean;
+      if (currentRoute.value === '/') {
+        routeValue = true
+      } else {
+        routeValue = false
+      }
+      return { currentRoute,routeValue }
+    }
   });
 </script>
 
@@ -45,11 +57,22 @@ export default defineComponent ({
 }
 .links a {
   margin : 10px;
+  /* position: relative; */
 }
 a {
   text-decoration : none;
   color : black;
   font-size : 20px;
+  position: relative;
+}
+.active::after {
+  content : ' ';
+  width : 100%;
+  height : 5px;
+  background-color : steelblue;
+  position : absolute;
+  bottom : -5px;
+  left : 0px;
 }
 
 </style>
