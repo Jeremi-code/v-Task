3babb5c01f4b310f1e844b52ea71e0548acf6365
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-01-31 23:31:45 -0800

    Update taskType interface name

--- backend/src/types/taskType.ts
+++ backend/src/types/taskType.ts
@@ -1,5 +1,5 @@
 import {Document} from "mongoose"
-export interface DTask extends Document {
+export interface task extends Document {
     text : string,
     day : string,
     reminder : boolean,
