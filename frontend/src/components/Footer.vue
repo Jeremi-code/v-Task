4e2340eb8c70ada31517c2818337e6383f7899d7
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-02-04 01:31:54 -0800

    created a Footer componenet

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <p>Task Tracker &copy 2024</p>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent ({
+    name : 'Footer',
+    
+})
+</script>
+
+<style>
+    footer {
+        text-align: center;
+    }
+</style>
