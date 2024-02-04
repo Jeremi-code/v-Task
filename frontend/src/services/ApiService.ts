67a9692c2d862b19cb36c99878db76a9b5169bb9
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-02-04 01:06:40 -0800

    add white space between functions to improve readability

--- frontend/src/services/ApiService.ts
+++ frontend/src/services/ApiService.ts
@@ -1,53 +1,63 @@
-import {task} from '../types/TaskType'
+import { task } from "../types/TaskType";
 
-const fetchTasks = async (status : any) => {
-  const res = await fetch(`/api/tasks?status=${status}`)
-  const data: task[] = await res.json()
-  return data
-}
- const addTasks = async (task:task) => {
-   const res : any = await fetch("/api/task",{
-     method : 'POST',
-     headers : {
-       'Content-Type':'application/json'
-     },
-     body : JSON.stringify(task)
-   })
-   const data : task =  await res.json()
-   return data
-}
+const fetchTasks = async (status: any) => {
+  const res = await fetch(`/api/tasks?status=${status}`);
+  const data: task[] = await res.json();
+  return data;
+};
 
- const deleteTask = async (id : string) => {
-    await fetch(`/api/task/${id}`, {
-     method : 'DELETE'
-   })
- }
- const fetchTask = async (id:string) => {
-   const res = await fetch(`/api/task/${id}`)
-   const data = await res.json()
-   return data
- }
- const toggleReminder = async (id:string) => {
-   const res = await fetch(`/api/task/${id}`, {
-     method : 'PUT',
-     headers : {
-        'Content-Type':'application/json'
-      },
-     body : JSON.stringify({reminder : true}), 
-   })
-   const data : task = await res.json()
-   return data
- }
- const toggleIsCompleted = async (id:string) => {
-  const res = await fetch(`/api/task/${id}`, {
-    method : 'PUT',
-    headers : {
-      'Content-Type':'application/json'
+const addTasks = async (task: task) => {
+  const res: any = await fetch("/api/task", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-    body : JSON.stringify({isCompleted : true}),
-  })
-  const data : task = await res.json()
-  return data
-}
+    body: JSON.stringify(task),
+  });
+  const data: task = await res.json();
+  return data;
+};
 
- export {fetchTasks, addTasks, deleteTask, fetchTask, toggleReminder,toggleIsCompleted}
+const deleteTask = async (id: string) => {
+  await fetch(`/api/task/${id}`, {
+    method: "DELETE",
+  });
+};
+const fetchTask = async (id: string) => {
+  const res = await fetch(`/api/task/${id}`);
+  const data = await res.json();
+  return data;
+};
+
+const toggleReminder = async (id: string) => {
+  const res = await fetch(`/api/task/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ reminder: true }),
+  });
+  const data: task = await res.json();
+  return data;
+};
+
+const toggleIsCompleted = async (id: string) => {
+  const res = await fetch(`/api/task/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ isCompleted: true }),
+  });
+  const data: task = await res.json();
+  return data;
+};
+
+export {
+  fetchTasks,
+  addTasks,
+  deleteTask,
+  fetchTask,
+  toggleReminder,
+  toggleIsCompleted,
+};
