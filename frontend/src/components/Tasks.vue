ac6f0d6a7e7cb5d0573aaebb9fc85c3ad1397d81
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-02-04 01:02:37 -0800

    remove the style tag and format the code

--- frontend/src/components/Tasks.vue
+++ frontend/src/components/Tasks.vue
@@ -1,37 +1,39 @@
 <template>
-    <div>
-        <div v-for="Task in Tasks" v-bind:key="Task._id">
-            <Task :Task="Task" :onToggle="onToggle" :onDelete="onDelete" :onCompleted="onCompleted"/>
-        </div>
+  <div>
+    <div v-for="Task in Tasks" v-bind:key="Task._id">
+      <Task
+        :Task="Task"
+        :onToggle="onToggle"
+        :onDelete="onDelete"
+        :onCompleted="onCompleted"
+      />
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
-import Task from "./Task.vue"
-import { task } from '../types/TaskType';
-export default defineComponent ({
-    name : 'Tasks',
-    components: {Task},
-    props : {
-        Tasks : Array as PropType<task[]>,
-        onToggle:{
-            required :false,
-            type : Function,
-            default : null
-        },
-        onDelete: {
-            type : Function,
-            required : true
-        },
-        onCompleted : {
-            type : Function,
-            required : true
-        }
-    }
-})
+import { defineComponent, PropType } from "vue";
+import Task from "./Task.vue";
+import { task } from "../types/TaskType";
+export default defineComponent({
+  name: "Tasks",
+  components: { Task },
+  props: {
+    Tasks: Array as PropType<task[]>,
+    onToggle: {
+      required: false,
+      type: Function,
+      default: null,
+    },
+    onDelete: {
+      type: Function,
+      required: true,
+    },
+    onCompleted: {
+      type: Function,
+      required: true,
+    },
+  },
+});
 </script>
 
-<style>
-
-</style>
