5572381083ca8a6e52c77ba6e96ead384d07350e
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-02-06 00:03:59 -0800

    Add Footers component

---
+++ frontend/src/components/Footers.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <p>Task Tracker &copy; 2024</p>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent ({
+    name : "Footers",
+    components:{},
+    setup(){
+      return {}
+    }   
+})
+</script>
+
+<style>
+    footer {
+        text-align: center;
+    }
+</style>
