d0a673a5ba3bafa220a7b4eecea850599d3d9df4
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-02-10 22:38:15 -0800

    remove irrelevant comment

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,7 @@
 
 import {createRouter,createWebHistory,RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
-import Done_Tasks from '../views/Done_Tasks.vue'; // Update the path based on your project structure
-
+import Done_Tasks from '../views/Done_Tasks.vue'; 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -12,9 +11,8 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/Done_Tasks',
     name: 'Done_Tasks',
-    component: Done_Tasks, // Update the component name as needed
+    component: Done_Tasks, 
   }
-  // Add more routes as needed
 ];
 
 const router = createRouter({
